e4941ad62bd87be20865b45238ad84e2cd0d480a
Leah Queen <leah@codegeek.net>
2021-11-29 13:45:11 -0700

    Removed not needed files and created a WP api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // State object that holds the todos once they are fetched from the api
+  state = {
+    posts: []
+  }
+  // Life cycle method: Executed when the component is mounted to the DOM, fetches the json api
+  componentDidMount() {
+    fetch('https://freshwebstg.wpengine.com/wp-json/wp/v2/posts')
+    .then(res => res.json())
+    .then((data) => {
+      this.setState({ posts: data })
+    })
+    .catch(console.log)
+  }
+  // JSX using Bootstrap 
+  render() {
+    return (
+      <div className="container">
+        <div className="col-xs-12">
+           <h1>My Posts</h1>
+           {this.state.posts.map((posts) => (
+                <div className="card">
+                   <div className="card-body">
+                       <h5 className="card-title">{posts.title.rendered}</h5>
+                       <div dangerouslySetInnerHTML={{ __html: posts.content.rendered }} ></div>
+                   </div>
+                </div>
+            ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
